f00ca58cc2a5270cb01cbabdad4a6756e645736f
stellarsoftech <stellarsoftech@gmail.com>
2020-04-28 16:43:59 +0530

    Script Commit 1234

--- git.sh
+++ git.sh
@@ -3,5 +3,5 @@ git add .
 git commit -m "Script Commit 1234"
 git push origin master
 git push originlocal master
-rm -rf ../build/public
-cp -frp build -T ../build/public
+rm -rf ../buildfront/public
+cp -frp build -T ../buildfront/public
